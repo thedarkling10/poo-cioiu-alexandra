8a45547406ee6503cda7772eb6e1977bb9c2b4ef
thedarkling10 <122387123+thedarkling10@users.noreply.github.com>
2025-03-31 16:26:35 +0300

    v1.1

---
+++ Character.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cioiu on 3/31/2025.
+//
+
+#include "Character.h"
---
+++ Character.h
@@ -0,0 +1,16 @@
+//
+// Created by cioiu on 3/31/2025.
+//
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+
+
+class Character {
+
+};
+
+
+
+#endif //CHARACTER_H
